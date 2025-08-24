a52fdd1eab4d427487da38956a2c6712b024a64e
Anderson <anderson@labdoor.com>
2025-08-24 08:46:56 +0100

    PV

--- scripts/render.js
+++ scripts/render.js
@@ -1,3 +1,25 @@
+Handlebars.registerHelper('equals', function(lvalue, rvalue, options) {
+    if (arguments.length < 3) {
+        throw new Error("Handlebars Helper equals needs 2 parameters");
+    }
+    if (lvalue && lvalue == rvalue) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
+Handlebars.registerHelper('contains', function(lvalue, rvalue, options) {
+    if (arguments.length < 3) {
+        throw new Error("Handlebars Helper contains needs 2 parameters");
+    }
+    if (lvalue && lvalue.includes(rvalue)) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
 async function loadTemplate(url) {
   const res = await fetch(url);
   return await res.text();
@@ -7,14 +29,14 @@ async function renderTemplates(data) {
     const name = "Palavra Viva Church";
 
   // Load templates
-  const headerTpl = await loadTemplate('templates/header.hbs');
-  const footerTpl = await loadTemplate('templates/footer.hbs');
+  const headerTpl = await loadTemplate('/templates/header.hbs');
+  const footerTpl = await loadTemplate('/templates/footer.hbs');
 
   // Compile
-  const headerHTML = Handlebars.compile(headerTpl)({ });
-  const footerHTML = Handlebars.compile(footerTpl)({ year: "2025", siteName: name + " - Londres" });
+  const headerHTML = Handlebars.compile(headerTpl)(data);
+  const footerHTML = Handlebars.compile(footerTpl)({ ...data, year: "2025", siteName: name + " - Londres" });
 
   // Inject
-  document.getElementById('header').innerHTML = headerHTML;
-  document.getElementById('footer').innerHTML = footerHTML;
+  if (document.getElementById('header')) document.getElementById('header').innerHTML = headerHTML;
+  if (document.getElementById('footer')) document.getElementById('footer').innerHTML = footerHTML;
 }
